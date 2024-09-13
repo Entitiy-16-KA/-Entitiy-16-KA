660d075e43ba19ef0b7e26363f1800cfeda66e39
Entitiy-16-KA <erkanokurlar5@gmail.com>
2024-09-13 20:16:19 +0300

    Add files via upload

---
+++ DBS_ATTACK/DBS_Attack.py
@@ -0,0 +1,76 @@
+import os
+import subprocess
+import time
+from colorama import Fore, Style, init
+
+
+print(Fore.YELLOW + "Yapan: Entitiy 16 KA" + Style.RESET_ALL)
+time.sleep(1)
+print(Fore.YELLOW + "@Entitiy_16_KA" + Style.RESET_ALL)
+print(" ")
+time.sleep(2)
+print(Fore.RED + "WARNING!:" + Style.RESET_ALL)
+time.sleep(1)
+print(Fore.GREEN + "WE ARE NOT RESPONSIBLE AND DO NOT ACCEPT ANY LIABILITY FOR ANY MALFUNCTIONS OF THIS PROGRAM!" + Style.RESET_ALL)
+time.sleep(2)
+print(Fore.BLUE + "Database is being renewed..." + Style.RESET_ALL)
+time.sleep(4)
+print(" ")
+print(" ")
+print(" ") 
+
+dbs = input(Fore.GREEN + "Which database will be compromised?  " + Style.RESET_ALL)
+
+print(Fore.RED +"The database is infiltrated..." + Style.RESET_ALL)
+time.sleep(7)
+print(Fore.BLUE + "[+] Infiltration is being carried out..." + Style.RESET_ALL)
+time.sleep(1)
+print(Fore.RED + "[<>] The infiltration process has started.." + Style.RESET_ALL)
+time.sleep(9)
+print(Fore.GREEN + "[+] The infiltration process was successfully carried out" + Style.RESET_ALL)
+time.sleep(3)
+print(" ")
+print(Fore.RED + "Write ls!" + Style.RESET_ALL)
+print(" ")
+And = input(Fore.LIGHTBLUE_EX + "Shell=> " + Style.RESET_ALL)
+time.sleep(3)
+print(" ")
+time.sleep(3)
+print("www")
+time.sleep(3)
+print("root")
+time.sleep(1)
+print("Local disk")
+time.sleep(5)
+print("file")
+print(" ")
+time.sleep(2)
+print(Fore.GREEN + "Steal information: -launch" + Style.RESET_ALL)
+print(Fore.RED + "Destroy database: -VERF" + Style.RESET_ALL)
+
+hi = input(Fore.BLUE + "Shell--> " + Style.RESET_ALL)
+time.sleep(9)
+print(" ")
+print(Fore.GREEN + "Process is starting..." + Style.RESET_ALL)
+time.sleep(4)
+print(Fore.RED + "Do you approve the process to start? [Y/N]" + Style.RESET_ALL)
+
+he = input(" ")
+
+if he == "Y":
+    open = r"test\pitbuks.mp4"
+    subprocess.run([open], shell=True)
+
+
+if he == "N":
+    open = r"test\pitbuks.mp4"
+    subprocess.run([open], shell=True)
+
+
+if he == "y":
+    open = r"test\pitbuks.mp4"
+    subprocess.run([open], shell=True)
+
+if he == "n":
+    open = r"test\pitbuks.mp4"
+    subprocess.run([open], shell=True)
